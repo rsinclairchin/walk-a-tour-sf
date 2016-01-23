a7ade594523ee7288bf3c2b7e0ee44309a9df654
Luis De Castro <luisdecastro510@gmail.com>
2016-01-23 11:55:59 -0800

    complete base migrations

--- db/migrate/20160123113441_create_highlights.rb
+++ db/migrate/20160123113441_create_highlights.rb
@@ -1,4 +1,13 @@
 class CreateHighlights < ActiveRecord::Migration
   def change
+    create_table :highlights do |t|
+      t.string :title
+      t.integer :lat, null: false
+      t.integer :long, null: false
+      t.string :audio
+      t.integer :picture
+      t.references (:user, :tour)
+      t.timestamps
+    end
   end
 end
