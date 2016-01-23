a6a0efc657b851aa84c19ac9365bdaa89c054fcf
Luis De Castro <luisdecastro510@gmail.com>
2016-01-23 13:35:49 -0800

    add geocode

--- db/migrate/20160123113441_create_highlights.rb
+++ db/migrate/20160123113441_create_highlights.rb
@@ -2,12 +2,15 @@ class CreateHighlights < ActiveRecord::Migration
   def change
     create_table :highlights do |t|
       t.string :title
-      t.integer :lat, null: false
-      t.integer :long, null: false
+      t.string :category
+      t.float :latitude
+      t.float :longitude
       t.string :audio
       t.integer :picture
       t.references (:user, :tour)
       t.timestamps
+      geocoded_by :full_street_address
+      after_validation :geocode
     end
   end
 end
