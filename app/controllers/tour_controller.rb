e8d1f057d9264bc3c0908ced9638711e2ba3b7b8
natasha-t <nthapli@gmail.com>
2016-01-23 15:06:57 -0800

    Tour controller routes

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -1,41 +1,36 @@
 
 #index  display a list of all tours
+get '/categories' do
+  @tours = Tour.all
+  @categories = []
+  @tours.each do |tour|
+    tour.highlights.each do |highlight|
+      @categories << highlight.category
+    end
+  end
+  erb :'categories/show'
+end
+
 get '/tours' do
-  @tours = Tours.all
-  erb :index
+  @tours = Tours.find_by(params)
+  erb :'tours/index'
 end
 
-#new  return an HTML form for creating a new photo
-get '/tours/new' do
-  erb :new
-end
-
-#create create a new photo
-post '/tours' do
-  Tour.create(params)
-  redirect
-end
-
-
-#show display a specific photo
 get '/tours/:id' do
-  @tour = Tour.find(params)
+  @tour = Tour.find(params[:id])
+  erb :'tours/show'
 end
 
-#edit return an HTML form for editing a photo
-get '/tours/:id/edit'
-  erb '/edit'
+get '/tours/map' do
+
 end
 
-#update update a specific photo
+get '/tours/:id/end' do
+  @tour = Tour.find(params[:id])
+  erb :'tours/end'
+end
+
 put '/tours/:id' do
-  Tour.update(params)
-  redirect
-end
-
-#destroy  delete a specific photo
-destroy '/tours/:id' do
-  tour = Tour.find(params[:id])
-  tour.destroy
-  redirect
+  @tour = Tour.find(params[:id])
+  @tour.update_attributes(rating: params[:rating])
 end
