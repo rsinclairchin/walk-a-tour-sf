c8bb04413ab3b17c1bb6d468ad2ca9c99189f4e7
natasha-t <nthapli@gmail.com>
2016-01-23 15:31:52 -0800

    Add route for highlighter controller

--- app/controllers/highlight_controller.rb
+++ app/controllers/highlight_controller.rb
@@ -1,40 +1,6 @@
-#index  display a list of all highlights
-get '/highlights' do
-  @highlights = Highlight.all
-  erb :index
-end
-
-#new  return an HTML form for creating a new photo
-get '/highlights/new' do
-  erb :new
-end
-
-#create create a new photo
-post '/highlights' do
-  Highlight.create(params)
-  redirect
-end
-
-
-#show display a specific photo
+#show display a specific highlight
 get '/highlights/:id' do
-  @highlight = Highlight.find(params)
+  @highlight = Highlight.find(params[:id])
+  erb :'highlights/show'
 end
 
-#edit return an HTML form for editing a photo
-get '/highlights/:id/edit'
-  erb '/edit'
-end
-
-#update update a specific photo
-put '/highlights/:id' do
-  Highlight.update(params)
-  redirect
-end
-
-#destroy  delete a specific photo
-destroy '/highlights/:id' do
-  highlight = Highlight.find(params[:id])
-  highlight.destroy
-  redirect
-end
