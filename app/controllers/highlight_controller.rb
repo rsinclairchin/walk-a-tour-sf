a7935aa68b9b1d7c6050a96ba1cbe0fdff8f059d
natasha-t <nthapli@gmail.com>
2016-01-23 14:09:43 -0800

    Refactor routes

--- app/controllers/highlight_controller.rb
+++ app/controllers/highlight_controller.rb
@@ -1,40 +1,39 @@
-
-#index  display a list of all tours
-get '/tours' do
-  @tours = Tours.all
+#index  display a list of all highlights
+get '/highlights' do
+  @highlights = Highlight.all
   erb :index
 end
 
 #new  return an HTML form for creating a new photo
-get '/tours/new' do
+get '/highlights/new' do
   erb :new
 end
 
 #create create a new photo
-post '/tours' do
-  Tour.create(params)
+post '/highlights' do
+  Highlight.create(params)
   redirect
 end
 
 
 #show display a specific photo
-get '/tours/:id' do
+get '/highlights/:id' do
   @tour = Tour.find(params)
 end
 
 #edit return an HTML form for editing a photo
-get '/tours/:id/edit'
+get '/highlights/:id/edit'
   erb '/edit'
 end
 
 #update update a specific photo
-put '/tours/:id' do
+put '/highlights/:id' do
   Tour.update(params)
   redirect
 end
 
 #destroy  delete a specific photo
-destroy '/tours/:id' do
+destroy '/highlights/:id' do
   tour = Tour.find(params[:id])
   tour.destroy
   redirect
