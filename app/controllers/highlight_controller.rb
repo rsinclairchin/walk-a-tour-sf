5d930381d7b5c06b08de86313087d93410ec6ba5
natasha-t <nthapli@gmail.com>
2016-01-23 20:55:18 -0800

    add clyp mp3

--- app/controllers/highlight_controller.rb
+++ app/controllers/highlight_controller.rb
@@ -8,9 +8,7 @@ end
 put '/highlights/:id' do
   @highlight = Highlight.find(params[:id])
   @track = Clyp::TrackUpload.new(file: File.new(params[:audio][:tempfile]), title: 'Test', description: '#cool test')
-  p @track
   @highlight.update_attributes(audio: @track)
   redirect "/highlights/#{@highlight.id}"
 end
-
 
